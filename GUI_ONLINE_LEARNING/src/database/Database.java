9492669154b369044fa09a1683a9b80caf62b850
justwannacodee <98950227+justwannacodee@users.noreply.github.com>
2024-05-06 21:43:13 +0700

    update

--- GUI_ONLINE_LEARNING/src/database/Database.java
+++ GUI_ONLINE_LEARNING/src/database/Database.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class Database {
     private static String driver = "com.mysql.cj.jdbc.Driver";
-    private static String url = "jdbc:mysql://192.168.1.103:3306/onlinelearningdb   ";
+    private static String url = "jdbc:mysql://192.168.1.103:3306/onlinelearningdb";
     private static String username = "admin";
     private static String password = "admin";
     public static Connection mycon() {
